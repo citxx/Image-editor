9b46f4251fe043ff5d4bc52f8092923b974414ca
Artem Tabolin <artemtab@yandex.ru>
2011-09-23 00:25:06 +0400

    Selection frame is added

--- src/ImageShowArea.cpp
+++ src/ImageShowArea.cpp
@@ -1,4 +1,44 @@
 #include "ImageShowArea.hpp"
 
-ImageShowArea::ImageShowArea(QWidget *parent): QGraphicsView(parent) {
+#include <QDebug>
+
+ImageShowArea::ImageShowArea(QWidget *parent):
+    QGraphicsView(parent),
+    frame(NULL),
+    selectionActive(false) {
+    this->setMouseTracking(false);
+    this->frame = new QRubberBand(QRubberBand::Rectangle, this);
+}
+
+ImageShowArea::~ImageShowArea() {
+    delete this->frame;
+}
+
+void ImageShowArea::updateSelection() {
+    int left   = qMin(this->selectionStart.x(), this->selectionEnd.x());
+    int right  = qMax(this->selectionStart.x(), this->selectionEnd.x());
+    int top    = qMin(this->selectionStart.y(), this->selectionEnd.y());
+    int bottom = qMax(this->selectionStart.y(), this->selectionEnd.y());
+    this->frame->setGeometry(left, top, right - left + 1, bottom - top + 1);
+    this->frame->show();
+}
+
+void ImageShowArea::mousePressEvent(QMouseEvent *event) {
+    this->selectionActive = true;
+    this->selectionStart = event->pos();
+    this->selectionEnd = this->selectionStart;
+    this->updateSelection();
+}
+
+void ImageShowArea::mouseMoveEvent(QMouseEvent *event) {
+    if (this->selectionActive) {
+        this->selectionEnd = event->pos();
+        this->updateSelection();
+    }
+}
+
+void ImageShowArea::mouseReleaseEvent(QMouseEvent *event) {
+    this->selectionActive = false;
+    this->selectionEnd = event->pos();
+    this->updateSelection();
 }
--- src/ImageShowArea.hpp
+++ src/ImageShowArea.hpp
@@ -1,13 +1,31 @@
 #ifndef IMAGE_SHOW_AREA_HPP
 #define IMAGE_SHOW_AREA_HPP
 
+#include <QMouseEvent>
 #include <QGraphicsView>
+#include <QGraphicsItem>
+#include <QRubberBand>
+#include <QRect>
 
 class ImageShowArea: public QGraphicsView {
     Q_OBJECT
 
+    QRubberBand *frame;
+    QPoint selectionStart;
+    QPoint selectionEnd;
+    bool selectionActive;
+
+    void updateSelection();
+
+  protected:
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+
   public:
     ImageShowArea(QWidget *parent = NULL);
+    ~ImageShowArea();
+
 };
 
 #endif  // IMAGE_SHOW_AREA_HPP
